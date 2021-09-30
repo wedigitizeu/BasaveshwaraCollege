f3e3bc23a537ca73929842ad2e7b0ab5e126759c
Sagar <lifeofsrg@gmail.com>
2021-09-30 09:52:22 +0530

    chnage width

--- includes/callAction.php
+++ includes/callAction.php
@@ -43,7 +43,7 @@
                         <div class="col-xs-12 col-sm-6 col-md-4 col-lg-4">
                                             <br>
                                             <br>
-                        <iframe src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d31100.889128719682!2d77.53754147681367!3d12.996705160836227!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m3!3m2!1d13.0215792!2d77.5504773!4m5!1s0x3bae3dd955555555%3A0x7ef25f5503a2a46a!2sbasaveshwara%20composite!3m2!1d12.970309799999999!2d77.53773939999999!5e0!3m2!1sen!2sin!4v1632974956167!5m2!1sen!2sin" width="1000" height="350" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+                        <iframe src="https://www.google.com/maps/embed?pb=!1m26!1m12!1m3!1d31100.889128719682!2d77.53754147681367!3d12.996705160836227!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m11!3e6!4m3!3m2!1d13.0215792!2d77.5504773!4m5!1s0x3bae3dd955555555%3A0x7ef25f5503a2a46a!2sbasaveshwara%20composite!3m2!1d12.970309799999999!2d77.53773939999999!5e0!3m2!1sen!2sin!4v1632974956167!5m2!1sen!2sin" width="750" height="350" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
 
 </div>
 
